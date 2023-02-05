b7ab85a0e6189c1ede40cde04d40221b87161092
conceptroot <conceptroot@gmail.com>
2023-02-06 01:19:36 +0530

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) return []
+  // console.log('1️⃣ matrix=', matrix)
+  let result = []
+  matrix.forEach((v,i) => {
+    if (i % 2 === 0) {
+      // вернуть исходный массив
+      result.push(v)
+    } else {
+      // вернуть развернутый массив
+      result.push(v.reverse())
+    }
+  })
+  // console.log('🔥 result=', result.flat())
+  return result.flat()
 }
